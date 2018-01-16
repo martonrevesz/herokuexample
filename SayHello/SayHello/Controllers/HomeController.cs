48412e536de79e055a7b28e3506eb344f725db70
Marton Revesz <martonrevesz@gmail.com>
2018-01-16 11:53:10 +0100

    feat(): create

---
+++ SayHello/SayHello/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SayHello.Models;
+
+namespace SayHello.Controllers
+{
+    [Route("[controller]")]
+    public class HelloController : Controller
+    {
+        [Route("[action]")]
+        public IActionResult Index()
+        {
+            var container = new Container();
+            return View(container);
+        }
+    }
+}
---
+++ SayHello/SayHello/Models/Container.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SayHello.Models
+{
+
+    public class Container
+    {        
+        public string[] Hellos { get; set; } = {"Mirëdita", "Ahalan", "Parev", "Zdravei", "Nei Ho", "Dobrý den", "Ahoj", "Goddag", "Goede dag, Hallo", "Hello", "Saluton", "Hei", "Bonjour",
+                "Guten Tag", "Gia'sou", "Aloha", "Shalom", "Namaste", "Namaste", "Jó napot", "Halló", "Helló", "Góðan daginn", "Halo", "Aksunai", "Qanuipit", "Dia dhuit",
+                "Salve", "Ciao", "Kon-nichiwa", "An-nyong Ha-se-yo", "Salvëte", "Ni hao", "Dzien' dobry", "Olá", "Bunã ziua", "Zdravstvuyte", "Hola", "Jambo", "Hujambo", "Hej",
+                "Sa-wat-dee", "Merhaba", "Selam", "Vitayu", "Xin chào", "Hylo", "Sut Mae", "Sholem Aleychem", "Sawubona"};
+    }
+}
